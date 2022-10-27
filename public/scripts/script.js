b7ab0d441233660699815cda315c33d2ef3bfa09
Artur Baumeister <Artur.baumeister@gmail.com>
2022-10-27 17:47:57 +0200

    Test and get familiar with  editor.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,3 +5,46 @@ window.addEventListener(
   },
   false
 );
+
+//* ???: This is just for testing purpose and no get familiar with it. Can be removed from this line */
+const editor = new EditorJS({
+  placeholder: 'Write something awesome here!',
+
+  tools: {
+    header: {
+      class: Header,
+      config: {
+        placeholder: 'Überschrift hinzufügen...'
+      }
+    },
+    image: {
+      class: SimpleImage,
+      inlineToolbar: true,
+      config: {
+        placeholder: 'Paste image URL'
+      }
+    },
+    list: List,
+    Marker: {
+      class: Marker,
+      shortcut: 'CMD+SHIFT+M'
+    }
+    // inlineCode: {
+    //   class: InlineCode,
+    //   shortcut: 'CMD+SHIFT+M'
+    // }
+  },
+  onReady: () => {
+    console.log('Editor.js is ready to work!');
+  }
+});
+/*
+editor
+  .save()
+  .then((outputData) => {
+    console.log('Article data: ', outputData);
+  })
+  .catch((error) => {
+    console.log('Saving failed: ', error);
+  });
+*/
