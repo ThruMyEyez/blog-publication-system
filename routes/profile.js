3502b8538c28bbe0998704b95b13c24e5cd8b79a
Artur Baumeister <Artur.baumeister@gmail.com>
2022-11-01 17:46:02 +0100

    Add /profile/edit GET and POST functionality

--- routes/profile.js
+++ routes/profile.js
@@ -23,18 +23,52 @@ const router = new Router();
 //* So far, so god ✅
 router.get('/', (req, res, next) => {
   if (req.user) {
-    console.log(req.user._id);
     res.redirect(`/profile/${req.user._id}`);
   } else {
     res.redirect('/authentication/sign-in');
   }
 });
 
+//* So far, so god ✅
 router.get('/edit', routeGuard, (req, res, next) => {
-  const { userId } = req.session.userId;
+  if (!req.user) {
+    res.redirect('/authentication/sing-in');
+  }
+  User.findById(req.user._id)
+    .populate('profile')
+    .then((user) => {
+      console.log(user);
+      res.render('profile/edit', user);
+    })
+    .catch((error) => {
+      console.log(`Error loading user profile from DB: ${error}`);
+      next(error);
+    });
 });
 
-router.post('/edit', routeGuard, (req, res, next) => {});
+//* So far, so god ✅ (base version => will be extended)
+router.post(
+  '/edit',
+  routeGuard,
+  upload.single('avatarUrl'),
+  (req, res, next) => {
+    req.body.avatarUrl = req.file.path;
+    console.log('POST OF /profile/edit', req.body);
+    //console.log(req.file.path);
+    User.findByIdAndUpdate(
+      req.user.id,
+      { ...req.body, $inc: { __v: 1 } },
+      { new: true }
+    )
+      .then((updatedProfile) => {
+        console.log('profile updated successfully!');
+        res.redirect('/profile/edit');
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
 
 //* So far, so god ✅
 //TODO Add "edit" button / anchor and route..
@@ -46,11 +80,10 @@ router.get('/:id', routeGuard, (req, res, next) => {
     .populate('profile')
     .then((user) => {
       const { createdAt } = user;
-      console.log(user);
       user.createdLocalDate = createdAt.toLocaleDateString();
       user.createdLocalTime = createdAt.toLocaleTimeString();
       user.actualLocalDate = new Date().toLocaleDateString();
-
+      user.isOwnProfile = req.user.id === id;
       res.render('profile/main', user);
     })
     .catch((error) => {
@@ -59,6 +92,7 @@ router.get('/:id', routeGuard, (req, res, next) => {
     });
 });
 
+//TODO Fix bug Cast to ObjectId failed for value "complete" (type string) at path "_id" for model "User"
 router.get('/complete', routeGuard, (req, res, next) => {
   const skills = [
     'HTML',
@@ -102,31 +136,43 @@ router.post(
   }
 );
 
-router.get('/edit', routeGuard, (req, res, next) => {
-  console.log(111111111111122222222222222222222222222);
-});
-router.post('/edit', routeGuard, (req, res, next) => {});
-
+//* So far, so god ✅
 router.post('/:id/follow', routeGuard, (req, res, next) => {
   //Follow
+  const { id } = req.params;
+  Follow.create({ follower: req.user._id, followee: id })
+    .then(() => {
+      res.redirect(`/profile/${id}`);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
+//TODO W.I.P.
 router.post('/:id/unfollow', routeGuard, (req, res, next) => {
   //Unfollow
+  const { id } = req.params;
+  Follow.findOneAndDelete({
+    follower: req.user._id,
+    followee: id
+  });
+});
+
+router.get('/delete', routeGuard, (req, res, next) => {
+  console.log('DELETE PROFILE PAGE');
+  res.render('profile/delete');
 });
 
 //*Tasks to do =>
-router.get('/edit', routeGuard, (req, res, next) => {});
-router.post('/edit', routeGuard, (req, res, next) => {});
-
-router.get('/delete', routeGuard, (req, res, next) => {});
-router.get('/delete', routeGuard, (req, res, next) => {});
+router.post('/delete', routeGuard, (req, res, next) => {});
 router.get('/about-me', (req, res, next) => {});
-router.get('/follow-list', (req, res, next) => {});
+router.get('/follow-list', routeGuard, (req, res, next) => {});
 
 router.get('/my-history', (req, res, next) => {});
 router.get('/my-comments', (req, res, next) => {});
 
+module.exports = router;
 // router.post('/sign-up', (req, res, next) => {
 //   const { username, email, password, user_type } = req.body;
 //   bcryptjs
@@ -186,5 +232,3 @@ router.get('/my-comments', (req, res, next) => {});
 //       next(error);
 //     });
 // });
-
-module.exports = router;
