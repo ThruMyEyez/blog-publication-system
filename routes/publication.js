4b539c7dd6f6fe22e3fea63aa1ed2fe2340ea1ac
Sedeqi <a3449@basisit.de>
2022-11-12 16:03:40 +0100

    final version  before presentation

--- routes/publication.js
+++ routes/publication.js
@@ -57,7 +57,7 @@ router.get('/', (req, res, next) => {
               : false;
         });
       });
-      console.log('Created:', publications[0].createdLocalDate);
+      // console.log('Created:', publications[0].createdLocalDate);
       res.render('publications/main', { articles: publications });
     })
     .catch((error) => {
